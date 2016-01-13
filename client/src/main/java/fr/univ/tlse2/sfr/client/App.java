6876bb977cf14b2851b826af0ac0123d2903215a
GaelFG <gael.fenet.garde@gmail.com>
2016-01-13 12:23:06 +0100

    implementation de kryo

--- client/src/main/java/fr/univ/tlse2/sfr/client/App.java
+++ client/src/main/java/fr/univ/tlse2/sfr/client/App.java
@@ -2,6 +2,13 @@ package fr.univ.tlse2.sfr.client;
 
 import java.io.IOException;
 
+import com.esotericsoftware.kryonet.Client;
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+
+import fr.univ.tlse2.sfr.communication.DemarrerSimulation;
+import fr.univ.tlse2.sfr.communication.EnregistreurKryo;
+import fr.univ.tlse2.sfr.communication.MessageTexte;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
@@ -13,12 +20,15 @@ import javafx.fxml.FXMLLoader;
 
 public class App extends Application {
 	
+	private Client connecteur_kryo;
 	private Stage primary_stage;
 	private BorderPane root_layout;
 	
 	
 	@Override
 	public void start(Stage primaryStage) {
+		initialiser_connecteur_kryo("127.0.0.1", 8073);
+		connecteur_kryo.sendTCP(new DemarrerSimulation("mon test réseau !"));
 		this.primary_stage = primaryStage;
         this.primary_stage.setTitle("IHM Simulation Flotte Robots");
         this.init_root_layout();
@@ -56,5 +66,29 @@ public class App extends Application {
 	
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	private void initialiser_connecteur_kryo(String url_serveur, int port_tcp) {
+		connecteur_kryo = new Client();
+		connecteur_kryo.start();
+		EnregistreurKryo.enregistrerLesClassesDeCommunication(connecteur_kryo.getKryo());
+		definir_ecouteur_kryo();
+		try {
+			connecteur_kryo.connect(5000, url_serveur, port_tcp);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	private void definir_ecouteur_kryo() {
+		connecteur_kryo.addListener(new Listener() {
+		       public void received (Connection connection, Object object) {
+		          if (object instanceof MessageTexte) {
+		        	  MessageTexte message = (MessageTexte)object;
+		             System.out.println("message serveur : " + message.get_contenu());
+		          }
+		       }
+		    });
 	}
 }
