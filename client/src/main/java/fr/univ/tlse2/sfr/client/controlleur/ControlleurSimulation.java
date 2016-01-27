849eca62d3f36ca448277e137ad0da93a14240e0
GaelFG <gael.fenet.garde@gmail.com>
2016-01-27 15:01:39 +0100

    Here come the bugs

--- client/src/main/java/fr/univ/tlse2/sfr/client/controlleur/ControlleurSimulation.java
+++ client/src/main/java/fr/univ/tlse2/sfr/client/controlleur/ControlleurSimulation.java
@@ -14,11 +14,16 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
+import javafx.scene.transform.Rotate;
 
 /**
  * Controlleur de la frame simulation
  */
 public class ControlleurSimulation {
+	
+	private final static int DEMIE_LARGEUR_ROBOT = 20;
+	private final static int DEMIE_HAUTEUR_ROBOT = 50;
+	
 	@FXML
 	private Button play;
 	@FXML
@@ -36,7 +41,7 @@ public class ControlleurSimulation {
 	 * The constructor (is called before the initialize()-method).
 	 */
 	public ControlleurSimulation() {
-		sprite_blatte_a = new Image("/blatte.jpg", true);
+		sprite_blatte_a = new Image("/blatte.png", true);
 	}
 	
 	/**
@@ -76,12 +81,50 @@ public class ControlleurSimulation {
     	}
 	}
 	
+	 /**
+     * Sets the transform for the GraphicsContext to rotate around a pivot point.
+     *
+     * @param gc the graphics context the transform to applied to.
+     * @param angle the angle of rotation.
+     * @param px the x pivot co-ordinate for the rotation (in canvas co-ordinates).
+     * @param py the y pivot co-ordinate for the rotation (in canvas co-ordinates).
+     */
+    private void rotate(GraphicsContext gc, double angle, double px, double py) {
+        Rotate r = new Rotate(angle, px, py);
+        gc.setTransform(r.getMxx(), r.getMyx(), r.getMxy(), r.getMyy(), r.getTx(), r.getTy());
+    }
+	
+	/**
+     * Draws an image on a graphics context.
+     *
+     * The image is drawn at (tlpx, tlpy) rotated by angle pivoted around the point:
+     *   (tlpx + image.getWidth() / 2, tlpy + image.getHeight() / 2)
+     *
+     * @param gc the graphics context the image is to be drawn on.
+     * @param angle the angle of rotation.
+     * @param tlpx the top left x co-ordinate where the image will be plotted (in canvas co-ordinates).
+     * @param tlpy the top left y co-ordinate where the image will be plotted (in canvas co-ordinates).
+     */
+    private void drawRotatedImage(GraphicsContext gc, Image image, double angle, double tlpx, double tlpy, double demi_largeur, double demi_hauteur) {
+        gc.save(); // saves the current state on stack, including the current transform
+        rotate(gc, angle, tlpx + image.getWidth() / 2, tlpy + image.getHeight() / 2);
+        gc.drawImage(image, tlpx, tlpy);
+        gc.restore(); // back to original state (before rotation)
+    }
+	
 	private void dessiner_robots(List<EtatRobot> robots){
 		GraphicsContext gc = canvas_simulation.getGraphicsContext2D();
 		gc.setFill(Color.YELLOW);
 		for(EtatRobot etat : robots){
-            gc.fillRect(etat.pos_robot.x*10, etat.pos_robot.y*10, 10, 10);
-            //gc.drawImage(sprite_blatte_a, etat.pos_robot.x*10, etat.pos_robot.y*10);
+			drawRotatedImage(gc, sprite_blatte_a, etat.orientation_robot+90, etat.pos_robot.x*10, etat.pos_robot.y*10, DEMIE_LARGEUR_ROBOT, DEMIE_HAUTEUR_ROBOT);
+			/*
+			gc.save();
+			gc.translate(etat.pos_robot.x*10, etat.pos_robot.y*10);
+			gc.rotate(etat.orientation_robot);
+			//gc.drawImage(sprite_blatte_a, 0, 0, 10, 20);
+			gc.fillRect(-DEMIE_LARGEUR_ROBOT, -DEMIE_HAUTEUR_ROBOT, DEMIE_LARGEUR_ROBOT, DEMIE_HAUTEUR_ROBOT);
+			gc.restore();
+			*/
         }
 	}
 }
